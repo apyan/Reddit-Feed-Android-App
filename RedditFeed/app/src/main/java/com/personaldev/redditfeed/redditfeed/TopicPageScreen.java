45d211b0476710c136f1a76812b3e3e9996ec05e
Alvin <apyan7852@gmail.com>
2018-01-29 03:33:02 -0800

    Application Base
    The base and setup for the application.

---
+++ RedditFeed/app/src/main/java/com/personaldev/redditfeed/redditfeed/TopicPageScreen.java
@@ -0,0 +1,13 @@
+package com.personaldev.redditfeed.redditfeed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopicPageScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic_page_screen);
+    }
+}
